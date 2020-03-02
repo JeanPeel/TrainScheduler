d77bb21ce6d889a826e1802454a2ae7db4426b03
JeanPeel <Jeanine.Peel@gmail.com>
2020-03-01 23:22:32 -0700

    collect values, push to FBase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,44 @@
-// connect Firebase
+// connect to Fire base (FBase)
 
-const config = {
+var config = {
     apiKey : "AIzaSyBN6MzlnuG2gazwDmfDIKKJBQ_TtL5qUO4",
     authDomain : "train-scheduler-cd22f.firebaseio.com/",
     databaseURL : "https://train-scheduler-cd22f.firebaseio.com/"
 };
 
 firebase.initializeApp(config);
-
-const database = firebase.database();
+var database = firebase.database();
 
 // Train Scheduler
 
+// Submit Button
+
+$("#submit").on("click", function (event){
+    event.preventDefault();
+    console.log("Submit has been clicked.")
+
+    // Collects the Values of user inputs
+    var trainName = $("#train-name").val().trim();
+    var destinationInput = $("#destination").val().trim();
+    var firstTrainTime = $("#first-train-time").val().trim();
+    var frequencyInput = $("#frequency").val().trim();
+    
+    console.log(trainName + " | " + destinationInput + " | " + firstTrainTime + " | " + frequencyInput);
+    
+    // Creates local "temporary object for holding the train schedule"
+
+    var newTrain = {
+        train: trainName,
+        destination: destinationInput,
+        firsTrain: firstTrainTime,
+        frequency: frequencyInput
+    };
+
+    console.log(newTrain);
+
+    // uploads Scheduled Train to FBase
+    database.ref().push(newTrain);
+
+    console.log("New Train's Information Pushed to Database");
+
+})
