74aa1472eb09e8bf688862a693f6393c0b3f25bb
JeanPeel <Jeanine.Peel@gmail.com>
2020-03-02 00:05:49 -0700

    added trip class

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,20 +18,22 @@ $("#submit").on("click", function (event){
     console.log("Submit has been clicked.")
 
     // Collects the Values of user inputs
+    var tripClass = $("#trip-class").val().trim();
     var trainName = $("#train-name").val().trim();
     var destinationInput = $("#destination").val().trim();
     var firstTrainTime = $("#first-train-time").val().trim();
     var frequencyInput = $("#frequency").val().trim();
     var descriptionInput = $("#description").val().trim();
     
-    console.log(trainName + " | " + destinationInput + " | " + firstTrainTime + " | " + frequencyInput + " | " + descriptionInput);
+    console.log(tripClass + " | " + trainName + " | " + destinationInput + " | " + firstTrainTime + " | " + frequencyInput + " | " + descriptionInput);
     
     // Creates local "temporary object for holding the train schedule"
 
     var newTrain = {
+        classification: tripClass,
         train: trainName,
         destination: destinationInput,
-        firsTrain: firstTrainTime,
+        firstTrain: firstTrainTime,
         frequency: frequencyInput,
         description: descriptionInput
     };
@@ -41,6 +43,24 @@ $("#submit").on("click", function (event){
     // uploads Scheduled Train to FBase
     database.ref().push(newTrain);
 
+    // log new train info to console
+
+    console.log(newTrain.classification);
+    console.log(newTrain.train);
+    console.log(newTrain.destination);
+    console.log(newTrain.firstTrain);
+    console.log(newTrain.frequency);
+    console.log(newTrain.description);
+
     console.log("New Train's Information Pushed to Database");
+
+    // clear all the text boxes
+
+    $(tripClass).val("");
+    $(trainName).val("");
+    $(destinationInput).val("");
+    $(firstTrainTime).val("");
+    $(frequencyInput).val("");
+    $(descriptionInput).val("");
 
 })
